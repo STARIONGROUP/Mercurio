06320a3f6af16728b431343f68c85b9ad6c45a0f
antoineatstariongroup <a.theate@stariongroup.eu>
2025-08-06 08:46:40 +0200

    Code coverage

--- Mercurio.Hosting.Tests/MessagingBackgroundServiceTestFixture.cs
+++ Mercurio.Hosting.Tests/MessagingBackgroundServiceTestFixture.cs
@@ -40,6 +40,7 @@ namespace Mercurio.Hosting.Tests
         private const string ConfiguredConnectionName = "RabbitMQ";
         private Mock<IConfiguration> configurationMock;
         private TestMessagingBackgroundService backgroundService;
+        private ServiceProvider serviceProvider;
 
         [SetUp]
         public void Setup()
@@ -62,23 +63,22 @@ namespace Mercurio.Hosting.Tests
 
             serviceCollection.AddScoped<IMessageClientService, MessageClientService>();
             serviceCollection.AddLogging();
-            var serviceProvider = serviceCollection.BuildServiceProvider();
+            this.serviceProvider = serviceCollection.BuildServiceProvider();
             
-            this.backgroundService = new TestMessagingBackgroundService(serviceProvider, serviceProvider.GetRequiredService<ILogger<TestMessagingBackgroundService>>(), this.configurationMock.Object);
+            this.backgroundService = new TestMessagingBackgroundService(this.serviceProvider, this.serviceProvider.GetRequiredService<ILogger<TestMessagingBackgroundService>>(), this.configurationMock.Object);
         }
 
         [TearDown]
         public void Teardown()
         {
             this.backgroundService.Dispose();
+            this.serviceProvider.Dispose();
         }
 
         [Test]
         public async Task VerifyBackgroundServiceBehaviour()
         {
             using var cancellationTokenSource = new CancellationTokenSource();
-            cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(2));
-
             _ = this.backgroundService.StartAsync(cancellationTokenSource.Token);
             
             string[] messages = ["ABC", "DEF", "GHI"];
@@ -88,18 +88,71 @@ namespace Mercurio.Hosting.Tests
                 this.backgroundService.PushMessage(message,new FanoutExchangeConfiguration("BackgroundTest"), cancellationToken: cancellationTokenSource.Token);
                 await Task.Delay(TimeSpan.FromMilliseconds(300), cancellationTokenSource.Token);
             }
-            
-            await Task.Delay(TimeSpan.FromMilliseconds(1500), CancellationToken.None);
 
+            await Task.Delay(TimeSpan.FromMilliseconds(900), CancellationToken.None);
+
+            await cancellationTokenSource.CancelAsync();
+            
             using (Assert.EnterMultipleScope())
             {
                 Assert.That(this.backgroundService.ReceivedMessages, Has.Count.EqualTo(3));
                 Assert.That(this.backgroundService.ReceivedMessages, Is.EquivalentTo(messages));
             }
-
-            await this.backgroundService.StopAsync(CancellationToken.None);
         }
         
+        [Test]
+        public async Task VerifyBackgroundServiceBehaviourPushMultiple()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource();
+            _ = this.backgroundService.StartAsync(cancellationTokenSource.Token);
+            
+            string[] messages = ["ABC", "DEF", "GHI"];
+
+            this.backgroundService.PushMessages(messages,new FanoutExchangeConfiguration("BackgroundTest"), cancellationToken: cancellationTokenSource.Token);
+            await Task.Delay(TimeSpan.FromMilliseconds(1000), CancellationToken.None);
+
+            await cancellationTokenSource.CancelAsync();
+            
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(this.backgroundService.ReceivedMessages, Has.Count.EqualTo(3));
+                Assert.That(this.backgroundService.ReceivedMessages, Is.EquivalentTo(messages));
+            }
+        }
+
+        [Test]
+        public async Task VerifyInvalidInitialization()
+        {
+            var invalidService = new InvalidInitializationBackgroundService(this.serviceProvider, this.serviceProvider.GetRequiredService<ILogger<InvalidInitializationBackgroundService>>(), this.configurationMock.Object);
+            await Assert.ThatAsync(() => invalidService.StartAsync(CancellationToken.None), Throws.InvalidOperationException);
+        }
+
+        private class InvalidInitializationBackgroundService : MessagingBackgroundService
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="MessagingBackgroundService" />
+            /// </summary>
+            /// <param name="serviceProvider">
+            /// The injected <see cref="IServiceProvider" /> that allow to resolve
+            /// <see cref="IMessageClientService" /> instance, even if not registered as scope
+            /// </param>
+            /// <param name="logger">The injected <see cref="ILogger{TCategory}" /> to allow logging</param>
+            /// <param name="configuration">The injected <see cref="IConfiguration" /> to provides configuration information for service initialization</param>
+            public InvalidInitializationBackgroundService(IServiceProvider serviceProvider, ILogger<InvalidInitializationBackgroundService> logger, IConfiguration configuration) : base(serviceProvider, logger, configuration)
+            {
+            }
+
+            /// <summary>
+            /// Initializes this service (e.g. to set the <see cref="ConnectionName" /> and fill <see cref="Subscriptions" />
+            /// collection
+            /// </summary>
+            /// <returns>An awaitable <see cref="Task" /></returns>
+            protected override Task InitializeAsync()
+            {
+                return Task.CompletedTask;
+            }
+        }
+
         private class TestMessagingBackgroundService: MessagingBackgroundService
         {
             /// <summary>
